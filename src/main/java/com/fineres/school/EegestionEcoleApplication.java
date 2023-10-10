f6e94eb315765c2e92ef56cfc1e055b1ad2cc31d
finere-Dem <finere.dem@email.com>
2023-10-10 17:18:58 +0200

    init

---
+++ src/main/java/com/fineres/school/EegestionEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.fineres.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EegestionEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EegestionEcoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fineres/school/EegestionEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fineres.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EegestionEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
